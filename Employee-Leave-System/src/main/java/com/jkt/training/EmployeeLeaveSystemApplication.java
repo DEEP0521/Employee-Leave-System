61320fb9f47ecdc8add488c2ddb0beb1d92ac1ae
DEEPSHIKHA ROY <roymum05@gmail.com>
2020-05-14 19:17:57 +0530

    Leave System

---
+++ Employee-Leave-System/src/main/java/com/jkt/training/EmployeeLeaveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jkt.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLeaveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLeaveSystemApplication.class, args);
+	}
+
+}
