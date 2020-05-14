fadc0fce259c4c49f38598195a72330af255d399
DEEPSHIKHA ROY <roymum05@gmail.com>
2020-05-14 21:07:52 +0530

    Files added

---
+++ Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/exception/NotFoundfException.java
@@ -0,0 +1,10 @@
+package com.jkt.training.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class NotFoundfException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/repository/ManagerRepository.java
@@ -0,0 +1,9 @@
+package com.jkt.training.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jkt.training.model.Manager;
+
+public interface ManagerRepository extends JpaRepository<Manager, Integer> {
+
+}
