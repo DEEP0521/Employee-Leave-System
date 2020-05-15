e656903745635d2250a3ef265d7603287b7cb813
Srasheed <61048530+Sumayya-maven@users.noreply.github.com>
2020-05-15 18:10:07 +0530

    EntityEmployee done

--- Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/model/Employee.java
+++ Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/model/Employee.java
@@ -1,5 +1,42 @@
 package com.jkt.training.model;
-
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
 public class Employee {
-
+  @Id
+   private int EmpId;
+   private String EmpName;
+   private String EmpDept;
+   private Date DateOfJoining;
+   
+   public int getEmpId() {
+	return EmpId;
+   }
+   public void setEmpId(int empId) {
+	EmpId = empId;
+   }
+   public String getEmpName() {
+	return EmpName;
+   }
+   public void setEmpName(String empName) {
+	EmpName = empName;
+   }
+   public String getEmpDept() {
+	return EmpDept;
+   }
+   public void setEmpDept(String empDept) {
+	EmpDept = empDept;
+   }
+   public Date getDateOfJoining() {
+	return DateOfJoining;
+   }
+   public void setDateOfJoining(Date dateOfJoining) {
+	DateOfJoining = dateOfJoining;
+   }
+@Override
+public String toString() {
+	return "Employee [EmpId=" + EmpId + ", EmpName=" + EmpName + ", EmpDept=" + EmpDept + ", DateOfJoining="
+			+ DateOfJoining + "]";
+}
 }
