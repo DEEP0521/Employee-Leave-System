07494df8166f7af05211dd161cd514e9774bc90d
Faisal-debug <faisal3000khan@gmail.com>
2020-05-18 00:15:51 +0530

    mapping done

--- Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/controller/EmployeeController.java
+++ Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/controller/EmployeeController.java
@@ -1,5 +1,54 @@
 package com.jkt.training.com.jkt.training.controller;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jkt.training.com.jkt.training.model.Employee;
+import com.jkt.training.com.jkt.training.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
-
+@Autowired
+	private EmployeeService empser;
+	
+	@PostMapping(path="/employees",consumes = "application/json")
+	public String addEmp(@RequestBody Employee employee)
+	{
+		empser.addEmp(employee);
+		System.out.println(employee.toString());
+		return "New Employee Added";
+	}
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees()
+	{
+		return empser.getAllEmployees();
+	}
+	
+	@GetMapping("/employees/{EmpId}")
+	public Optional<Employee> getEmpbyId(@PathVariable int EmpId)
+	{
+		return empser.getEmpbyId(EmpId);
+	}
+	
+	
+	@PutMapping(path="/employees/{EmpId}",consumes="application/json")
+	public String updateEmp(@RequestBody Employee emp, @PathVariable int EmpId)
+	{
+		empser.updateEmp(emp, EmpId);
+		return "Employee Updated";
+	}
+	@DeleteMapping("/employees/{EmpId}")
+	public String deleteEmp(@PathVariable int EmpId)
+	{
+		empser.deleteEmp(EmpId);
+		return "Employee deleted";
+	}
 }
--- Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/service/EmployeeService.java
+++ Employee-Leave-System/src/main/java/com/jkt/training/com/jkt/training/service/EmployeeService.java
@@ -1,5 +1,47 @@
 package com.jkt.training.com.jkt.training.service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jkt.training.com.jkt.training.model.Employee;
+import com.jkt.training.com.jkt.training.repository.EmployeeRepository;
+@Service
 public class EmployeeService {
-
+  @Autowired
+   private EmployeeRepository emprepo;
+	
+	public void addEmp(Employee employee)
+	{
+		emprepo.save(employee);
+	}
+	
+	public List<Employee> getAllEmployees()
+	{
+		return emprepo.findAll();
+	}
+	//emp by id
+	public Optional<Employee> getEmpbyId(int EmpId)
+	{
+		Optional<Employee> emp = emprepo.findById(EmpId);
+		return emp;
+		
+	}
+	
+	
+	
+	public void updateEmp(Employee emp,int EmpId)
+	{
+	
+		if(emprepo.existsById(EmpId))
+			emprepo.save(emp);
+	
+	}
+	
+	public void deleteEmp(int EmpId)
+	{
+		Employee emp=emprepo.getOne(EmpId);
+		emprepo.delete(emp);
+	}
 }
